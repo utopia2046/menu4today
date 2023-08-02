4b253620d4027a7ee5f65d0253db091c03005e7d
utopia <utopia2046@hotmail.com>
2023-08-02 17:51:36 +0800

    update launch.json and add states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import FilterEdit from './filterEdit';
 import MealSelection from './mealSelect';
 import PeopleSelection from './peopleSelect';
 
 function App() {
+  const [people, setPeople] = useState('small');
+  const [meals, setMeals] = useState([{id: 0, type: 'lunch'}]);
+  const [filters, setFilters] = useState([]);
+
   function generateMenu() {}
+
+  function getPeople(option) {
+    setPeople(option);
+  }
+
+  function getMeals(meals) {
+    setMeals(meals);
+  }
 
   return (
     <div className="App">
       <h1>今天吃啥？</h1>
 
       <ul className="query-section">
-        <li><PeopleSelection /></li>
-        <li><MealSelection /></li>
+        <li><PeopleSelection people={people} onPeopleChange={getPeople} /></li>
+        <li><MealSelection meals={meals} onMealsChange={getMeals} /></li>
         <li><FilterEdit /></li>
       </ul>
 
--- src/peopleSelect.js
+++ src/peopleSelect.js
@@ -1,8 +1,15 @@
-function PeopleSelection() {
+function PeopleSelection(props) {
+
+    function onSelectChange(evt) {
+        if (evt && evt.target) {
+            props.onPeopleChange(evt.target.value);
+        }
+    }
+
     return (
         <div className="people-select-container">
-            <label for="people-select">几个人？</label>
-            <select name="people" id="people-select" defaultValue={"small"}>
+            <label htmlFor="people-select">几个人？</label>
+            <select name="people" id="people-select" defaultValue={props.people} onChange={onSelectChange}>
                 <option value="single">一人食：一个人也要好好吃饭</option>
                 <option value="small">小家日常 (2 ~ 4 人)： 一羹一饭见巧思</option>
                 <option value="medium">朋友小聚 (5 ~ 7 人)：秀出你的实力吧</option>
