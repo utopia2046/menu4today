b6969f96f2d8ac8244d2b3d815d6102b8a5de9d5
utopia <utopia2046@hotmail.com>
2023-08-03 16:52:39 +0800

    add meal select event handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import './App.css';
 import FilterEdit from './filterEdit';
@@ -5,40 +6,76 @@ import MealSelection from './mealSelect';
 import PeopleSelection from './peopleSelect';
 
 function App() {
-  const [people, setPeople] = useState('small');
-  const [meals, setMeals] = useState([{id: 0, type: 'lunch'}]);
-  const [filters, setFilters] = useState([]);
+    const [people, setPeople] = useState('small');
+    const [meals, setMeals] = useState([newMeal()]);
+    const [filters, setFilters] = useState([]);
 
-  function generateMenu() {}
+    function generateMenu() {}
 
-  function getPeople(option) {
-    setPeople(option);
-  }
+    function getPeople(option) {
+        setPeople(option);
+    }
 
-  function getMeals(meals) {
-    setMeals(meals);
-  }
+    function newMeal() {
+        return {
+            id: 'meal-' + nanoid(),
+            type: 'lunch'
+        }
+    }
 
-  return (
-    <div className="App">
-      <h1>今天吃啥？</h1>
+    function addMeal() {
+        setMeals([...meals, newMeal()])
+    }
 
-      <ul className="query-section">
-        <li><PeopleSelection people={people} onPeopleChange={getPeople} /></li>
-        <li><MealSelection meals={meals} onMealsChange={getMeals} /></li>
-        <li><FilterEdit /></li>
-      </ul>
+    function updateMeal(id, newType) {
+        const updatedMeals = meals.map((meal) => {
+            if (id === meal.id) {
+                return { ...meal, type: newType };
+            }
+            return meal;
+        });
+        setMeals(updatedMeals);
+    }
 
-      <div><button onClick={generateMenu}>生成</button></div>
+    function removeMeal(id) {
+        const remainingMeals = meals.filter((meal) => id !== meal.id);
+        setMeals(remainingMeals);
+    }
 
-      <div className="menu-section">
-      </div>
+    return (
+        <div className="App">
+            <h1>今天吃啥？</h1>
 
-      <div className="shopping-list-section">
-        
-      </div>
-    </div>
-  );
+            <ul className="query-section">
+                <li>
+                    <PeopleSelection
+                        people={people} 
+                        onPeopleChange={getPeople} 
+                    />
+                </li>
+                <li>
+                    <MealSelection 
+                        meals={meals} 
+                        onAddMeal={addMeal} 
+                        onRemoveMeal={removeMeal} 
+                        onUpdateMeal={updateMeal}
+                    />
+                </li>
+                <li>
+                    <FilterEdit />
+                </li>
+            </ul>
+
+            <div><button onClick={generateMenu}>生成</button></div>
+
+            <div className="menu-section">
+            </div>
+
+            <div className="shopping-list-section">
+                
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/mealSelect.js
+++ src/mealSelect.js
@@ -1,29 +1,29 @@
-function Meal() {
-    return (
-        <div className="meal-container">
+function MealSelection(props) {
+
+    function addMeal() {
+        props.onAddMeal();
+    }
+
+    const mealsList = props.meals.map(meal =>
+        <div key={meal.id} className="meal-container" >
             <span>
-                <label>计划几顿？</label>
-            </span>
-            <span>
-                <select defaultValue={"lunch"}>
+                <select defaultValue={meal.type} onChange={(evt) => props.onUpdateMeal(meal.id, evt.target.value)}>
                     <option value="breakfast">早饭</option>
                     <option value="lunch" >午饭</option>
                     <option value="dinner">晚饭</option>
                 </select>
             </span>
             <span>
-                <button>取消</button>
+                <button onClick={() => props.onRemoveMeal(meal.id)}>取消</button>
             </span>
         </div>
     );
-}
 
-function MealSelection() {
     return (
         <div className="meal-select-container">
-            
-            <Meal></Meal>
-            <button>添加</button>
+            <label>计划几顿？</label>
+            {mealsList}
+            <button onClick={addMeal}>添加</button>
         </div>
     );
 }
