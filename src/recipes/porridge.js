8301cf9a74d968f12846f79a8576434ed83584d0
utopia <utopia2046@hotmail.com>
2023-08-23 16:34:37 +0800

    add porridge

---
+++ src/recipes/porridge.js
@@ -0,0 +1,509 @@
+export const porridge = [
+    {
+        'id': 'prg001',
+        'name': '白米粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 0,
+        'prepSteps': [
+            '',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '大米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg002',
+        'name': '糙米粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+            {
+                'name': '糙米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 30,
+        'prepSteps': [
+            '糙米加水泡 30 分钟',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '糙米，大米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg003',
+        'name': '燕麦粥',
+        'ingrds0': [
+            {
+                'name': '燕麦米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 30,
+        'prepSteps': [
+            '燕麦米加水泡 30 分钟',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '燕麦米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '如果是压扁的那种燕麦片的话，直接加水煮开就好，可以加牛奶',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg004',
+        'name': '小米粥',
+        'ingrds0': [
+            {
+                'name': '小米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 0,
+        'prepSteps': [],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '小米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg005',
+        'name': '紫米粥 黑米粥',
+        'ingrds0': [
+            {
+                'name': '紫米 (黑米)',
+            },
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 30,
+        'prepSteps': [
+            '紫米加水泡 30 分钟',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '紫米，大米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg006',
+        'name': '玉米粥',
+        'ingrds0': [
+            {
+                'name': '玉米面',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 0,
+        'prepSteps': [],
+        'cookDurInMin': 5,
+        'cookSteps': [
+            '玉米面加水搅成糊状',
+            '烧开水，一边慢慢倒入玉米糊一边搅拌，变稠即可',
+        ],
+        'comments': '',
+        'utensil': '煮锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg007',
+        'name': '棒碴粥',
+        'ingrds0': [
+            {
+                'name': '棒碴(粗玉米粒)',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 0,
+        'prepSteps': [],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '棒碴和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg008',
+        'name': '地瓜粥',
+        'ingrds0': [
+            {
+                'name': '地瓜(红薯)',
+            },
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 3,
+        'prepSteps': [
+            '地瓜削皮切块',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '地瓜，大米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '地瓜粥利于排气，最好避免在重要的社交场合前吃，当然大家一起吃一起放飞也可以^-^',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': ['社交不友好'],
+    },
+    {
+        'id': 'prg009',
+        'name': '南瓜粥',
+        'ingrds0': [
+            {
+                'name': '南瓜',
+            },
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 3,
+        'prepSteps': [
+            '南瓜削皮切块',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '南瓜，大米和水按刻度放入电饭锅',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg010',
+        'name': '红豆粥',
+        'ingrds0': [
+            {
+                'name': '红豆',
+            },
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 480,
+        'prepSteps': [
+            '红豆加水放冰箱冷藏室泡一夜，挑去硬石种子(泡不涨的那种)',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '红豆，大米和水按刻度放入电饭锅，加半小勺小苏打',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [''],
+        'warnings': [],
+    },
+    {
+        'id': 'prg011',
+        'name': '绿豆粥',
+        'ingrds0': [
+            {
+                'name': '绿豆',
+            },
+            {
+                'name': '大米',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 480,
+        'prepSteps': [
+            '绿豆加水放冰箱冷藏室泡一夜，挑去硬石种子(泡不涨的那种)',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '绿豆，大米和水按刻度放入电饭锅，加半小勺小苏打',
+            '按煮粥档煮',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': ['夏天'],
+        'warnings': [],
+    },
+    {
+        'id': 'prg012',
+        'name': '灰豆汤',
+        'ingrds0': [
+            {
+                'name': '灰豆子(炒熟的麻豌豆)',
+            },
+            {
+                'name': '干红枣',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '红枣洗净撕开去核',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '麻豌豆，红枣和水按刻度放入电饭锅，加半小勺小苏打',
+            '按煮粥档煮熟，加糖调味',
+        ],
+        'comments': '麻豌豆是西北特产，可以网购',
+        'utensil': '电饭锅',
+        'category': '甜食',
+        'tags': [],
+        'warnings': ['小众食材'],
+    },
+    {
+        'id': 'prg013',
+        'name': '小米山药粥',
+        'ingrds0': [
+            {
+                'name': '小米',
+            },
+            {
+                'name': '山药',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '山药去皮切块',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '小米，山药和水按刻度放入电饭锅',
+            '按煮粥档煮熟',
+        ],
+        'comments': '处理山药的时候最好戴手套',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+    {
+        'id': 'prg014',
+        'name': '皮蛋瘦肉粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+            {
+                'name': '皮蛋',
+            },
+            {
+                'name': '瘦肉',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '皮蛋剥皮切小块',
+            '瘦肉切片或切丝，加姜末、料酒、生抽、料油、盐腌入味',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '煮一锅白米粥',
+            '把皮蛋和瘦肉放进粥里烫熟即可',
+        ],
+        'comments': '',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+    {
+        'id': 'prg015',
+        'name': '青菜鱼片粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+            {
+                'name': '青菜',
+            },
+            {
+                'name': '鳕鱼',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '青菜洗净控干水份，切小段',
+            '鳕鱼切片，加姜丝、白胡椒、料酒、生抽、料油、盐腌入味',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '煮一锅白米粥',
+            '把青菜和鳕鱼片放进粥里烫熟即可，鱼片容易碎，搅的时候要温柔些',
+        ],
+        'comments': '任何种类的时令绿叶菜都可以',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+    {
+        'id': 'prg016',
+        'name': '青菜肉末粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+            {
+                'name': '青菜',
+            },
+            {
+                'name': '香菇肉酱',
+                'link': 'prck005',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '青菜洗净控干水份，切小段',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '煮一锅白米粥',
+            '把青菜放进粥里，加一大勺香菇肉酱，搅匀即可',
+        ],
+        'comments': '任何种类的时令绿叶菜都可以',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+    {
+        'id': 'prg017',
+        'name': '火腿冬瓜粥',
+        'ingrds0': [
+            {
+                'name': '大米',
+            },
+            {
+                'name': '冬瓜',
+            },
+            {
+                'name': '火腿',
+            },
+        ],
+        'ingrds1': [
+            {
+                'name': '姜丝',
+                'optional': true,
+            },
+            {
+                'name': '香葱末',
+                'optional': true,
+            },
+        ],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '冬瓜削皮切块',
+            '火腿切丁',
+        ],
+        'cookDurInMin': 30,
+        'cookSteps': [
+            '把大米，冬瓜，火腿放进电饭锅里，加水，加一勺盐，可以加姜丝，如果没有小孩儿反对的话',
+            '煮熟后撒白胡椒面，滴几滴香油，撒香葱末出锅',
+        ],
+        'comments': '我不爱香菜所以我不放，做饭的人就是可以这么任性',
+        'utensil': '电饭锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+    {
+        'id': 'prg018',
+        'name': '烩饭 珍珠翡翠白玉汤',
+        'ingrds0': [
+            {
+                'name': '剩米饭',
+            },
+            {
+                'name': '白菜叶',
+                'amount': '几片',
+            },
+            {
+                'name': '菠菜',
+                'amount': '1小把',
+            },
+            {
+                'name': '豆腐',
+                'amount': '半块',
+            },
+            {
+                'name': '剩菜',
+            },
+        ],
+        'ingrds1': [],
+        'prepDurInMin': 5,
+        'prepSteps': [
+            '白菜叶撕小块',
+            '菠菜洗净控干切段',
+            '豆腐切丁泡盐水去豆腥味',
+        ],
+        'cookDurInMin': 10,
+        'cookSteps': [
+            '葱花炝锅，加水煮开，加剩饭剩菜，豆腐，白菜菠菜叶，加盐调味',
+            '煮开后撒白胡椒面，滴几滴香油出锅',
+        ],
+        'comments': '处理剩饭剩菜的家常做法，咱不是皇上家不用那么讲究，泡豆腐不用刷家伙水，杂和菜汤也不用放到太阳下头晒冒泡了*^-^*',
+        'utensil': '炒锅',
+        'category': '主食',
+        'tags': [],
+        'warnings': [],
+    },
+];
+/*
+
+ */
